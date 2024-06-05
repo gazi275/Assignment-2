343334ff46931200ad752fb59c801327707e1bba
gazi275 <gaziazad270@gmail.com>
2024-06-05 23:05:26 +0600

    final commit

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { Productroute } from './Products/Products.route';
 import { OrderRoute } from './Orders/Order.route';
@@ -7,7 +7,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-
+app.use('/',async(req:Request,res:Response)=>{
+    res.send('server is running..!')
+})
 app.use('/api/products', Productroute);
 app.use('/api/orders', OrderRoute);
 
